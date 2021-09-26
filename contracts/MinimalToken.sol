73e02ceab17b54faecac853defdd8bfa5170b9a4
Defi Academy <72668835+DefiAcademy@users.noreply.github.com>
2021-09-26 22:36:56 +0100

    Update MinimalToken.sol

--- contracts/MinimalToken.sol
+++ contracts/MinimalToken.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.7;
 
 contract MinimalToken {
-    mapping(address => uint256) public balances; 
+    // address of token holder => balance 
+    mapping(address => uint256) public balances;
     
     uint256 public totalSupply;
     
-    constructor(){
+    constructor() {
         totalSupply = 3000;
         
         balances[msg.sender] = totalSupply / 3;
+        
         balances[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = totalSupply / 3;
-        balances[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = totalSupply / 3;
+        
+        balances[0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c] = totalSupply / 3;
     }
     
-    function transfer(address _to, uint256 _amount) public {
+    function transfer(address _to, uint256 _amount) external {
         require(balances[msg.sender] >= _amount, "Insufficient balance");
         balances[msg.sender] = balances[msg.sender] - _amount;
         balances[_to] = balances[_to] + _amount;
